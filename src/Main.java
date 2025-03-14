214d1d9a2f4d769cf63d5a7d531e08cfeb34a42b
mohkh833 <mohkh833@gmail.com>
2025-03-14 17:08:02 +0200

    add observer pattern

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import ChainOfResponsibility.*;
+import Observer.Editor;
+import Observer.EmailNotificationListener;
+import Observer.LogOpenListener;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,22 +9,33 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server();
-        server.register("admin@example.com", "admin_pass");
-        server.register("user@example.com", "user_pass");
+//        Server server = new Server();
+//        server.register("admin@example.com", "admin_pass");
+//        server.register("user@example.com", "user_pass");
+//
+//        Middleware middleware = Middleware.link(
+//                new ThrottlingMiddleware(2),
+//                new UserExistsMiddleware(server),
+//                new RoleCheckMiddleware()
+//        );
+//
+//        server.setMiddleware(middleware);
+//
+//        Client client = new Client(server);
+//        try {
+//            client.run();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
-        Middleware middleware = Middleware.link(
-                new ThrottlingMiddleware(2),
-                new UserExistsMiddleware(server),
-                new RoleCheckMiddleware()
-        );
+        Editor editor = new Editor();
+        editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
 
-        server.setMiddleware(middleware);
-
-        Client client = new Client(server);
         try {
-            client.run();
-        } catch (Exception e) {
+            editor.openFile("test.txt");
+            editor.saveFile();
+        } catch (Exception e){
             e.printStackTrace();
         }
     }
